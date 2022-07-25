34a211a8a88611effbd164c42fa605258aa0e67c
Faisal othman <Faisalalsneed@gmail.com>
2022-07-25 14:43:41 +0300

    Project

--- Nasel_Project/Nasel_Project/Nasel_Project/settings.py
+++ Nasel_Project/Nasel_Project/Nasel_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['nasel.herokuapp.com','127.0.0.1']
 
 
