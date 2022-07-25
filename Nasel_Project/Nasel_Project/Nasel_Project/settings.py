1583936567f34c5ac8d8602e373ec32bb09fbb22
Faisal othman <Faisalalsneed@gmail.com>
2022-07-25 14:45:37 +0300

    Project

--- Nasel_Project/Nasel_Project/Nasel_Project/settings.py
+++ Nasel_Project/Nasel_Project/Nasel_Project/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 DEBUG = os.environ.get('DJANGO_DEBUG', True)
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['nasel.herokuapp.com','127.0.0.1']
