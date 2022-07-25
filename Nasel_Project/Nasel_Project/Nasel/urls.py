850fff01d62fd1b691f63d0294cdcdf3c461417e
mhnd <mhnd@hno.com>
2022-07-25 12:38:19 +0300

    Update views

--- Nasel_Project/Nasel_Project/Nasel/urls.py
+++ Nasel_Project/Nasel_Project/Nasel/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("profile/delete/<profile_id>", views.delete_profile, name="delete_profile"),
     path("profile/update/<profile_id>", views.update_profile, name="update_profile"),
     # CRUD Comment
-    path("add/comment", views.add_comment, name="add_comment"),
+    path("comment/add", views.add_comment, name="add_comment"),
     path("comment/delete/<comment_id>", views.delete_comment, name="delete_comment"),
     path("comment/all", views.list_comment, name="list_comment"),
     path("comment/update/<comment_id>", views.update_comment, name="update_comment"),
@@ -23,8 +23,9 @@ urlpatterns = [
     path("order/all", views.list_Order, name="list_Order"),
     path("order/update/<Order_id>", views.update_Order, name="update_Order"),
     #Test CRUD
-    path("profilee/update/<slug>",views.update_profilee,name="update_profilee"),
-    path("profilee/delete/<slug>",views.test_delete_profile,name="test_delete_profile")
+    # path("profilee/update/<slug>",views.update_profilee,name="update_profilee"),
+    # path("profilee/delete/<slug>",views.test_delete_profile,name="test_delete_profile"),
+    # path('profile-details/<user_id>',views.profile_details,name='profile-details'),
 
 
 ]
