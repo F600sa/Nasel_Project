a9f8c6c90a66e707361df13403a3397b9bdc5801
Faisal othman <Faisalalsneed@gmail.com>
2022-07-26 16:06:56 +0300

    dd

--- Nasel_Project/Nasel_Project/Nasel_Project/settings.py
+++ Nasel_Project/Nasel_Project/Nasel_Project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts.apps.AccountsConfig',
     'Nasel.apps.NaselConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'Nasel_Project.urls'
@@ -141,3 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+CORS_ALLOW_ALL_ORIGINS = True
